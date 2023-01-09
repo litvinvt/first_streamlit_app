8ae4ea8b54970b2129d1bb9a5a21732c20601069
litvinvt <litvinvt@gmail.com>
2023-01-09 14:39:28 +0200

    Update streamlit_app.py
    sss

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner!!')
+
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
