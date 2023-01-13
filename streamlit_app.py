c95c8d3532202f3aadc78e7e0109b35ffd91bb76
litvinvt <litvinvt@gmail.com>
2023-01-13 09:02:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,12 @@ streamlit.header('🍌🥭 You select: 🥝🍇')
 
 streamlit.dataframe(fuits_to_show)
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
 
